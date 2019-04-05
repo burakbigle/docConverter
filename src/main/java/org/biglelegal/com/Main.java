86a92c7ef09a31e870f8403115e21716e350461e
Burak Kilic <burak@biglelegal.com>
2019-04-05 12:33:11 +0200

    Initial Commit for Document Converter

---
+++ src/main/java/org/biglelegal/com/Main.java
@@ -0,0 +1,13 @@
+package org.biglelegal.com;
+
+/**
+ * Initialized to create a document converter HTML-PDF-DOCX!
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
